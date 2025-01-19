3c0344940aed0b771edcda1fc32b7380025f030b
Keys-digital <eseogheneikoyo23@gmail.com>
2025-01-19 11:30:52 -0500

    Adding the HTML and Javascript files for the project: Working with Array Iteration for Restaurant Menu List.

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,21 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+// Display breakfast menu items
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+// Display main course menu items
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+// Display dessert menu items
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
