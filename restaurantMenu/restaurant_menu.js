f4b515230a6b216330040aadbb58bffa51aa599b
Keys-digital <eseogheneikoyo23@gmail.com>
2025-01-19 12:04:13 -0500

    Including prices for all the items in breatfastMenu, mainCourseMenu, and desertMenu by updating the restaurant_menu.js code with the prices of the items.

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict- $22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak -$12/lb', 'Pasta -$1.29/lb', 'Burger -$7', 'Salmon -20/lb'];
+const dessertMenu = ['Cake -$37', 'Ice Cream -$6.02', 'Pudding -$4', 'Fruit Salad -$6'];
 
 // Display breakfast menu items
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
